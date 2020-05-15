0c033381fa9d495c6f56246d693dadff70938d00
Renegade Gandhi <osinayaoludare@gmail.com>
2020-05-15 21:10:45 +0100

    online-tutor

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const lessonRoutes = require("./api/routes/lesson");
 const userRoutes = require("./api/routes/user");
 const subjectRoutes = require("./api/routes/subject");
 
+mongoose.connect("mongodb+srv://Gandhi:RGandhi@cluster0-hnuxn.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(_result => {
+console.log("Database connected");
+let port_number = app.listen(process.env.PORT || 3000);
+app.listen(port_number);
+}).catch(err => console.log(err));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 mongoose.set('useCreateIndex', true);
@@ -33,12 +40,7 @@ app.use((error, req, res, next) => {
     });
 });
 
-mongoose.connect("mongodb+srv://Gandhi:RGandhi@cluster0-hnuxn.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
-.then(_result => {
-console.log("Database connected");
-let port_number = app.listen(process.env.PORT || 3000);
-app.listen(port_number);
-}).catch(err => console.log(err));
+
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
